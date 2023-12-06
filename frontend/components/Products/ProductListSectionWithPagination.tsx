1a43460fcc7a81be5ad46f413b2bbd524f152666
devutkarsh7565 <utkarshtiwari200389@gmail.com>
2023-12-07 03:40:14 +0530

    demo commit

--- frontend/components/Products/ProductListSectionWithPagination.tsx
+++ frontend/components/Products/ProductListSectionWithPagination.tsx
@@ -1,34 +1,42 @@
-import { useFetchConditionalProductsApi } from "@/api/products/products.hook";
-import React, { useState } from "react";
-import ProductCard from "./ProductCard";
-import { IProduct } from "@/types/productTypes";
-import Pagination from "../Pagination/Pagination";
+// import { useFetchConditionalProductsApi } from "@/api/products/products.hook";
+// import React, { useState } from "react";
+// import ProductCard from "./ProductCard";
+// import { IProduct } from "@/types/productTypes";
+// import Pagination from "../Pagination/Pagination";
+
+// const ProductListSectionWithPagination = () => {
+//   const [page, setPage] = useState<number>(1);
+//   const { productData, isPreviousData } = useFetchConditionalProductsApi(page);
+//   console.log(productData, "data");
+
+//   const pageCount = Math.round(productData?.productCount / 5);
+
+//   return (
+//     <>
+//       <div className="w-full py-12 grid grid-cols-4 gap-y-10  2xl:grid-cols-5 mb-96">
+//         {productData?.product?.map((item: IProduct) => (
+//           <>
+//             <ProductCard data={item} />
+//           </>
+//         ))}
+//         <Pagination
+//           page={page}
+//           setPage={setPage}
+//           pageCount={pageCount}
+//           isPreviousData={isPreviousData}
+//         />
+//       </div>
+//       <div>hello</div>
+//     </>
+//   );
+// };
+
+// export default ProductListSectionWithPagination;
+
+import React from "react";
 
 const ProductListSectionWithPagination = () => {
-  const [page, setPage] = useState<number>(1);
-  const { productData, isPreviousData } = useFetchConditionalProductsApi(page);
-  console.log(productData, "data");
-
-  const pageCount = Math.round(productData?.productCount / 5);
-
-  return (
-    <>
-      <div className="w-full py-12 grid grid-cols-4 gap-y-10  2xl:grid-cols-5 mb-96">
-        {productData?.product?.map((item: IProduct) => (
-          <>
-            <ProductCard data={item} />
-          </>
-        ))}
-        <Pagination
-          page={page}
-          setPage={setPage}
-          pageCount={pageCount}
-          isPreviousData={isPreviousData}
-        />
-      </div>
-      <div>hello</div>
-    </>
-  );
+  return <div>ProductListSectionWithPagination</div>;
 };
 
 export default ProductListSectionWithPagination;
